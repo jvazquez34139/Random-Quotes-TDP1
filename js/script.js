ee4c79b160cd86b61080230865318327bb0ff42a
jose v <jvazquez34139@gmail.com>
2018-07-24 17:39:55 -0500

    bg color change and timer added

--- js/script.js
+++ js/script.js
@@ -31,13 +31,20 @@ function getRandomQuote(){
 }
 
 // Create the printQuote funtion and name it printQuote
-
+function randomHexColor(){
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor;
+}
 function printQuote(){
   var quote = getRandomQuote();
   document.getElementById("quote").innerHTML = quote.quote;
   document.getElementById("source").innerHTML = quote.source;
+  console.log(randomHexColor());
+  document.body.style.backgroundColor = "#" + randomHexColor();
 }
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+var newQuoteTimer = setInterval(printQuote,20000);
