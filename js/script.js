11070e8bca6b7bed9b772afefca7d00b931025d8
jose v <jvazquez34139@gmail.com>
2018-07-24 17:02:59 -0500

    basics complete

--- js/script.js
+++ js/script.js
@@ -1,16 +1,42 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
+var quotes = [
+  {
+    quote:"You must be the change you wish to see in the world.",
+    source:"Gandhi",
+  },
+  {
+    quote:"You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+    source:"Dr. Seuss",
+  },
+  {
+    quote:"Love all, trust a few, do wrong to none.",
+    source:"William Shakespeare",
+  },
+  {
+    quote:"Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    source:"Winston Churchill"
+  },
+  {
+    quote:"Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.",
+    source:"Lao Tzu"
+  }
+];
 
-
-
-// Create the getRandomQuuote function and name it getRandomQuote
-
-
+// Create the getRandomQuote function and name it getRandomQuote
+function getRandomQuote(){
+  var randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
 
 // Create the printQuote funtion and name it printQuote
 
-
+function printQuote(){
+  var quote = getRandomQuote();
+  document.getElementById("quote").innerHTML = quote.quote;
+  document.getElementById("source").innerHTML = quote.source;
+}
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
